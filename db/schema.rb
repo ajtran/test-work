2a5a86b571437b4488edfae92a482094d456160d
ajtran <ajtran@berkeley.edu>
2016-03-01 23:26:50 +0000

    done did my migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301231053) do
+ActiveRecord::Schema.define(version: 20160301231557) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160301231053) do
     t.string   "profession"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "sexuality"
   end
 
 end
